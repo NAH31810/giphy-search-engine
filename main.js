3be162e0c43df255850e3517cb3ed456d1f36573
Naomi Hernandez <Nahproductions@gmailcom>
2026-03-09 12:09:53 -0700

    feat:define API key and grab DOM elements for search functionality

---
+++ main.js
@@ -0,0 +1,6 @@
+//configuration
+const apiKey = "viizSmiK6vy6ijnmS31pse54fWItvOnm";
+const searchBtn = document.getElementById("search-btn");
+const searchInput = document.getElementById("user-input");
+const gifcontainer = document.getElementById("results-container");
+const clearBtn = document.getElementById("clear-btn");
