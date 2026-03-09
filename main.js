8b7141304d22666ddd4708adbe57ac8d3677359a
Naomi Hernandez <Nahproductions@gmailcom>
2026-03-09 12:46:17 -0700

    feat: implement Giphy search and result  rendering

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
-//configuration
+//Configuration
 const apiKey = "viizSmiK6vy6ijnmS31pse54fWItvOnm";
 const searchBtn = document.getElementById("search-btn");
 const searchInput = document.getElementById("user-input");
 const gifcontainer = document.getElementById("results-container");
 const clearBtn = document.getElementById("clear-btn");
+
+// Search Function
+async function getGifs() {
+  const searchTerm = searchInput.value;
+
+  // Safety: dont search if input is empty
+  if (!searchTerm) {
+    alert("Please enter search term first!");
+    return;
+  }
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}&limit=12&rating=g`;
+  try {
+    const response = await fetch(url);
+    const content = await response.json();
+
+    //Clear previous results
+    gifcontainer.innerHTML = "";
+
+    // Loop and Diaplay
+
+    ConstantSourceNode.data.forEach((gif) => {
+      let img = document.createElement("img");
+      img.src = gif.images.fixed_height.url;
+      img.alt = gif.title || "Giphy Result"; //Fallback if title is missing
+      img.classList.add("gif-item");
+      gifcontainer.appendChild(img);
+    });
+  } catch (error) {
+    console.error("Search Error:", error);
+  }
+}
+// Event Listeners
